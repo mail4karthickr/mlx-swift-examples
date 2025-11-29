44b51be6dbdd26a5c03ead8d9aecd66821a64ae3
Karthick <mail4karthickr@gmail.com>
2025-11-29 06:06:02 +0530

    Implement Language translation

---
+++ Applications/LanguageTranslater/LanguageTranslaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageTranslaterApp.swift
+//  LanguageTranslater
+//
+//  Created by Karthick Ramasamy on 28/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LanguageTranslaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
